95904fe1f854ad1bed40df3450d2414de009c30e
github username <github email address>
2016-12-07 11:58:52 -0500

    table no longer shows up if user has not input data when not logged in

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -32,7 +32,7 @@ class App extends Component {
           {((this.props.current.card && this.props.userAccess.showNewCard) || (this.props.current.user && this.props.current.user.credit_cards)) && <Form data={this.props.data} current={this.props.current} setValue={this.props.setValue} setCard={this.props.setCard} />}
         </div>
         <div className="six columns">
-          {(this.props.userAccess.showNewCard || this.props.current.user) && <Table data={this.props.data} current={this.props.current} />}
+          {((this.props.userAccess.showNewCard || this.props.current.user) && this.props.data.debt) && <Table data={this.props.data} current={this.props.current} />}
         </div>
       </div>)
     return (
