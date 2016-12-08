f0ae15421d4270eb4f3801529b202cbbd67c878a
github username <github email address>
2016-12-08 13:50:58 -0500

    users can now show and unshow forms by clicking navbar options

--- client/src/ducks/userAccess.js
+++ client/src/ducks/userAccess.js
@@ -1,19 +1,19 @@
 export default (state={}, action) => {
   switch (action.type) {
     case 'SET_INITIAL':
-      return {showSignIn: false, showSignUp: false, showNewCard: true, addPeriod: false, showPeriodList: false}
+      return {showSignIn: false, showSignUp: false, showNewCard: false, addPeriod: false, showPeriodList: false}
     case 'SIGN_IN':
       return Object.assign({}, state, {showSignIn: true})
     case 'SIGN_UP':
       return Object.assign({}, state, {showSignUp: true})
     case 'SHOW_NEW_CARD':
-      return Object.assign({}, state, {showNewCard: true})
+      return Object.assign({}, state, {showNewCard: !state.showNewCard})
     case 'ADD_PERIOD':
-      return Object.assign({}, state, {addPeriod: true})
+      return Object.assign({}, state, {addPeriod: !state.addPeriod})
     case 'SHOW_PERIODS':
-      return Object.assign({}, state, {showPeriodList: true})
+      return Object.assign({}, state, {showPeriodList: !state.showPeriodList})
     case 'ALL_FALSE':
-      return Object.assign({}, state, {showSignIn: false, showSignUp: false, showNewCard: false, addPeriod: false, showPeriodList: false})
+      return Object.assign({}, state, {showSignIn: false, showSignUp: false})
     default:
       return state
   }
